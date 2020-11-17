0f30b67e409bec5cba44b8173e5bb82fcf5b4bdc
VictorneoX <victorrodrigues753@gmail.com>
2020-11-17 19:04:01 -0300

    Handling course PUT request

--- index.js
+++ index.js
@@ -25,14 +25,9 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body); // result.error
+    if(error) {
+        res.status(400).send(error.details[0].message);
         return; 
     }
 
@@ -44,6 +39,28 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    if(!course) res.status(404).send(`Course with ID ${req.params.id} was not found`);
+
+    const { error } = validateCourse(req.body); // result.error
+    if(error) {
+        res.status(400).send(error.details[0].message);
+        return; 
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 // PORT
 const port = process.env.PORT || 3000
