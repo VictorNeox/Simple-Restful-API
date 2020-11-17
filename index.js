3d4dd59298978ba6748237c2d350a26bbd700552
VictorneoX <victorrodrigues753@gmail.com>
2020-11-17 18:41:37 -0300

    Course Post Route

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
     { id: 3, name: 'course3' },
-]
+];
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -18,6 +20,16 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(course => course.id === parseInt(req.params.id));
     if(!course) res.status(404).send(`Course with ID ${req.params.id} was not found`);
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
     res.send(course);
 });
 
