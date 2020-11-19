ed2fbd677283ecef704269ba5ba80521d596cbe1
VictorneoX <victorrodrigues753@gmail.com>
2020-11-18 21:30:39 -0300

    Environments

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const logger = require('./logger');
 const express = require('express');
 const app = express();
 
+if(app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled');
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
 
 app.use(logger.log);
 
