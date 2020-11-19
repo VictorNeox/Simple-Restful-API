94f766fd66b17a37fe377bc84b5e5f54cdbd0050
VictorneoX <victorrodrigues753@gmail.com>
2020-11-18 21:43:41 -0300

    Configuration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
@@ -14,6 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+
+// Configuration
+console.log(`Application name: ${config.get('name')}`);
+// console.log(`Mail Password: ${config.get('mail.password')}`);
 
 
 app.use(logger.log);
